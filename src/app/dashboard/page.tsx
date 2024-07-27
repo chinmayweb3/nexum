929203fca4c5115d966e1ab615fcb53418c3256d
chinmayweb3 <144161893+chinmayweb3@users.noreply.github.com>
2024-07-27 22:44:34 +0530

    "Added 'use client' directive and trpc import to dashboard page, with a new useQuery call and console log"

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,13 @@
+"use client";
 import PageWrapper from "@/components/PageWrapper";
 import React from "react";
+import { trpc } from "../_trpc/client";
 
 const Page = () => {
+  const t = trpc.getTodos.useQuery();
+
+  console.log("cosnt ", t);
+
   return <PageWrapper>Page</PageWrapper>;
 };
 
