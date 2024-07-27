16ce806be29586b3e2ba4bd223ee97f6771b7dd4
chinmayweb3 <144161893+chinmayweb3@users.noreply.github.com>
2024-07-27 16:47:14 +0530

    "Added prettier and prettier-plugin-tailwindcss dependencies, updated package-lock.json and package.json, and modified src/app/page.tsx"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,11 @@ import Image from "next/image";
 export default function Home() {
   return (
     <PageWrapper>
-      <section className="container self-center flex justify-center items-center ">
-        <h1 className="text-[64px] font-semibold">
+      <section className="container flex h-[80vh] items-center justify-center self-center">
+        <h1 className="w-[700px] text-center text-[56px] font-semibold leading-[1]">
           WELCOME TO THE <span className="text-destructive">LANDING PAGE</span>
         </h1>
       </section>
-      {/* <section className="min-h-screen bg-slate-700"></section> */}
     </PageWrapper>
   );
 }
