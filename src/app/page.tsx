741aebc0c93939308db0bbc789709034333c9ff7
chinmayweb3 <144161893+chinmayweb3@users.noreply.github.com>
2024-07-27 14:31:11 +0530

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <>
-      <div className=" container bg-red-300"> Hello world</div>
-    </>
-  );
+  return <section className="min-h-screen"></section>;
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <header className="relative">
+      <div className=" mx-auto fixed w-full  px-[20px]">
+        <div className="container bg-stone-500">center</div>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
