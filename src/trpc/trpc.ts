10b76ca2b6bbb0565660f65d8fbfaa454c3cb707
chinmayweb3 <144161893+chinmayweb3@users.noreply.github.com>
2024-07-29 12:38:52 +0530

    private procedure added

--- src/trpc/trpc.ts
+++ src/trpc/trpc.ts
@@ -1,6 +1,20 @@
-import { initTRPC } from "@trpc/server";
+import { currentUser } from "@clerk/nextjs/server";
+import { initTRPC, TRPCError } from "@trpc/server";
 
 const t = initTRPC.create();
 
+const isAuth = t.middleware(async (opts) => {
+  const user = await currentUser();
+
+  if (!user?.id || !user) {
+    throw new TRPCError({ code: "UNAUTHORIZED" });
+  }
+
+  return opts.next({
+    ctx: { userId: user.id, user: user },
+  });
+});
+
 export const router = t.router;
 export const publicProcedure = t.procedure;
+export const privateProcedure = t.procedure.use(isAuth);
