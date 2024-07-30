dee8d1c1142dd5514a4c080f5dabe8bdc42c7e65
chinmayweb3 <144161893+chinmayweb3@users.noreply.github.com>
2024-07-31 01:38:38 +0530

    "Added zod dependency and implemented validation for deleteFile trpc procedure"

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -2,6 +2,7 @@ import { currentUser } from "@clerk/nextjs/server";
 import { privateProcedure, publicProcedure, router } from "./trpc";
 import { TRPCError } from "@trpc/server";
 import db from "@/lib/prisma";
+import { z } from "zod";
 
 export const appRouter = router({
   getTodos: publicProcedure.query(async () => {
@@ -42,6 +43,20 @@ export const appRouter = router({
     });
     return files;
   }),
+
+  deleteFile: privateProcedure
+    .input(z.object({ id: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      const { userId } = ctx;
+      const file = await db.file.findFirst({ where: { id: input.id, userId } });
+      if (!file) throw new TRPCError({ code: "NOT_FOUND" });
+
+      await db.file.delete({
+        where: { id: input.id },
+      });
+
+      return file;
+    }),
 });
 
 export type AppRouter = typeof appRouter;
