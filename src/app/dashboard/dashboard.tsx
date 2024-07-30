e2e5fa51de0adf270c3f0c7efbbd565626b49cc8
chinmayweb3 <144161893+chinmayweb3@users.noreply.github.com>
2024-07-31 01:28:39 +0530

    "Added @radix-ui/react-avatar and date-fns dependencies, updated Dashboard component with new UI elements, and modified globals.css with custom destructive colors."

--- src/app/dashboard/dashboard.tsx
+++ src/app/dashboard/dashboard.tsx
@@ -2,8 +2,12 @@
 import React from "react";
 import UploadButton from "./uploadButton";
 import { trpc } from "../_trpc/client";
-import { Ghost } from "lucide-react";
+import { Ghost, MessageSquare, Plus, Trash } from "lucide-react";
 import { Skeleton } from "@/components/ui/skeleton";
+import { Separator } from "@/components/ui/separator";
+import { format } from "date-fns";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
 
 const Dashboard = () => {
   const { data: files, isLoading } = trpc.getUserFiles.useQuery();
@@ -14,13 +18,54 @@ const Dashboard = () => {
         <h1 className="text-[64px] font-bold text-primary">My Files</h1>
         <UploadButton />
       </div>
-      <div className="mb-[32px] mt-[40px] flex-grow rounded-md border border-border bg-background p-[20px]">
+      <Separator className="mt-[20px] bg-primary/30" orientation="horizontal" />
+      <div className="mb-[32px] mt-[20px] flex-grow rounded-md p-[20px]">
         {files && files.length !== 0 ? (
-          <div className="grid grid-cols-3 gap-[20px]">
-            {/* {
-              // files.sort((a,b) => a.)
-            } */}
-          </div>
+          <ul className="grid w-full grid-cols-3 gap-[20px]">
+            {files
+              .sort(
+                (a, b) =>
+                  new Date(b.createdAt).getTime() -
+                  new Date(a.createdAt).getTime(),
+              )
+              .map((i) => {
+                return (
+                  <li
+                    key={i.id}
+                    className="flex w-full flex-col rounded-md bg-popover shadow-sm"
+                  >
+                    <div className="flex items-center justify-start gap-[20px] px-[15px] py-[20px]">
+                      <Avatar>
+                        <AvatarImage src="https://github.com/shadcn.png" />
+                        <AvatarFallback>CN</AvatarFallback>
+                      </Avatar>
+
+                      <h3 className="text-lg font-medium text-primary/90">
+                        {i.name}
+                      </h3>
+                    </div>
+                    <Separator orientation="horizontal" />
+                    <div className="grid grid-cols-3 px-[15px] py-[10px] text-xs text-primary/70">
+                      <div className="flex items-center justify-center gap-2">
+                        <Plus className="h-4 w-4" />
+                        <p>{format(new Date(i.createdAt), "MMM yyyy")}</p>
+                      </div>
+                      <div className="flex items-center justify-center gap-2">
+                        <MessageSquare className="h-4 w-4" />
+                        <p>{i.uploadStatus}</p>
+                      </div>
+                      <Button
+                        size={"sm"}
+                        className="w-full"
+                        variant={"destructive"}
+                      >
+                        <Trash className="h-4 w-4" />
+                      </Button>
+                    </div>
+                  </li>
+                );
+              })}
+          </ul>
         ) : isLoading ? (
           <div className="flex flex-wrap justify-between gap-[30px]">
             <Skeleton className="h-[250px] basis-[30%]" />
